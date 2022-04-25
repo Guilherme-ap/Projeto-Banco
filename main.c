68a13a9220191d7af33842f1bf82b820cdc37a0b
Guilherme Alves <guilherme.alvespp17@gmail.com>
2022-04-25 01:39:05 -0300

    versao 5?

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+//nome:Davi Lima da Silva - Prontuário: GU3020371
+//nome:Guilherme Alves Pereira - Prontuário: GU3022021
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
@@ -15,9 +17,14 @@ CONTA coleta(){
     CONTA func;
     int *dados,x,mais_saldo;
     float soma_saldo;
-    char volta_menu;
 
-    dados = (int*) malloc (n * sizeof(int));
+    FILE *arquivo;
+    arquivo = fopen("dados.bin", "ab");
+    if(arquivo == NULL){
+        printf("Erro ao abrir o arquivo!\n");
+        exit(1);
+    }
+        dados = (int*) malloc (n * sizeof(int));
         system("cls");
         printf("Informe %dº nome \n",x+1);
         getchar();
@@ -30,6 +37,7 @@ CONTA coleta(){
         printf("1 - Conta corrente\n");
         printf("2 - Conta poupança\n");
         scanf("%d",&func.tipoConta);
+        fwrite(coleta,sizeof(int),100,arquivo);
         switch(func.tipoConta){
         case 1:
             system("cls");
@@ -42,45 +50,89 @@ CONTA coleta(){
             case 1:// switch case para adicionar saldo
                 printf("Quanto deseja adicionar?\n");
                 scanf("%f",&soma_saldo);
-                //funcao de delay
+                printf(".");
+                delay(3);
+                printf(".");
+                delay(5);
+                printf(".");
+                delay(5);
+                system("cls");
+               //funcao de delay
                 printf("\nSaldo adicionado com sucesso!\n");
+                delay(5);
+                soma_saldo= soma_saldo + func.saldo;
+                printf("Seu saldo agora é R$:%.2f\n",soma_saldo);
+                fclose(arquivo);
+                system("pause");
+                            break;
+
+                                case 2:
+                                    menu();
+                                    fclose(arquivo);
+                                    system("cls");
+
+                            break;
+                         default:
+                             fclose(arquivo);
+                             exit(1);
+                                }// fim da opção de adicionar saldo a conta
+
+
+        break;
+        case 2:
+            system("cls");
+            printf("O saldo da sua conta poupança é R$:%.2f\n",func.saldo);
+            printf("Deseja adicionar mais saldo?\n");
+            printf("1 - Sim\n");
+            printf("2 - Não\n");
+            scanf("%d", &mais_saldo);
+            switch(mais_saldo){
+            case 1:// switch case para adicionar saldo
+                printf("Quanto deseja adicionar?\n");
+                scanf("%f",&soma_saldo);
+                printf(".");
+                delay(3);
+                printf(".");
+                delay(5);
+                printf(".");
+                delay(5);
+                system("cls");
+               //funcao de delay
+                printf("\nSaldo adicionado com sucesso!\n");
+                delay(5);
                 soma_saldo= soma_saldo + func.saldo;
                 printf("Seu saldo agora é R$:%.2f\n",soma_saldo);
                 system("pause");
+                fclose(arquivo);
                             break;
                                 case 2:
-                                    system("cls");
-                                    printf("\nDeseja voltar para o menu?\n");
-                                    printf("Y/N\n");
-                                    scanf("%c", volta_menu);
-                                    if(volta_menu == 'Y'){
-                                            system("cls");
                                         menu();
-
-                                    }else {
-                                        exit(1);
-                                    }
-                             //   continue;
+                                        fclose(arquivo);
                             break;
-                         //  default:
-                              //  continue;
+                         default:
+                             fclose(arquivo);
+                             exit(1);
                                 }// fim da opção de adicionar saldo a conta
-        system("pause");
-        break;
-        case 2:
-            printf("O saldo da sua conta poupança é R$:%.2f\n",func.saldo);
         break;
         default:
+            fclose(arquivo);
             exit(1);
         }
-    return func;
-    system("cls");
     }
+void delay( int a){
+    int add, time, i;
+    time= a * 100000000;
+    for (i=0; i<time;i++){
+        add*=1;
+        add++;
+        add++;
+    }
+}
 void consultar_dados(int n){
     //segunda opção
 }
 void menu(){
-    int opcao,saida ;
+    int opcao=0,saida=1;
     FILE *file;
     file = fopen("davizola.bin", "rb");
      if(file == NULL){
@@ -95,39 +147,18 @@ void menu(){
     }
 
     printf("\n\n");
-    system("pause");
     fclose(file); // imagem de bem vindo
 
 
-    while(1){ // inicio do menu
+    while(saida){ // inicio do menu
     printf("\nDigite a opção desejada");
     printf("\n\n");
     printf("1 - Abrir conta");
     printf("\n2 - ver conta");
     printf("\n3 - sair\n");
     scanf("%d", &opcao);
-    switch(opcao){
-    case 1: //funcao de cadastro
-
-        saida++;
-    break;
-    case 2:
-    //funcao de visualização
-    saida++;
-    break;
-    case 3:
-        exit(1);
-        break;
-    default:
-        continue;
-    }
-    }
-
-}
-void main (){
-    setlocale(LC_ALL,"");
-    system("color F0");
-    menu();
+    system("cls");
+    if(opcao == 1){
     printf("\nDigite a quantidade de contas que será cadastrada\n");
     scanf("%d",&n);
     CONTA func[n];
@@ -135,5 +166,20 @@ void main (){
         func[i]= coleta();
         system("cls");
     }
+        saida++;
+        }else if(opcao == 2){
+
+        //funcao de visualização
+    }else{
+        exit(1);
+        }
+    }
+
+}
+void main (){
+    setlocale(LC_ALL,"");
+    system("color F0");
+    menu();
+
 
 }
